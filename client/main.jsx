e2b47f89660abacb16070e1daafd46cebcda1522
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-06-06 12:30:29 -0700

    Update UI

--- client/main.jsx
+++ client/main.jsx
@@ -6,20 +6,116 @@ import { render } from 'react-dom';
 import './main.html';
 
 
+
+
+// webppl.run("flip(.5)", function(s, x){console.log(x)})
+
+function renderAnswer(value) {
+  return value ? 'Yes.' : 'No.';
+}
+
+
+class History extends React.Component {
+
+  render() {
+    return (
+      <ul id="history">{
+        this.props.entries.map((obj) => {
+          return (
+            <li key={obj.id}>
+              <span className="questionText">{obj.questionText}</span> {' '}
+              <span className="answerText">{renderAnswer(obj.answerValue)}</span>
+            </li>);
+        })}
+      </ul>);
+  }
+
+}
+
+History.propTypes = {
+  entries: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+
+class Upcoming extends React.Component {
+
+  render() {
+    return (
+      <ul id="upcoming">{
+        this.props.entries.map((obj) => {
+          return <li key={obj.id}>{obj.questionText} (score {obj.score})</li>;
+        })}
+      </ul>);
+  }
+
+}
+
+Upcoming.propTypes = {
+  entries: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+
+class Question extends React.Component {
+
+  handleAnswer(e, value) {
+    this.props.processAnswer(value);
+  }
+
+  render() {
+    return (
+      <div id="question">
+        <div key={this.props.id} id="questionText">
+          {this.props.text}
+        </div>
+        <div id="answerInputs">
+          <input type="button" value="yes" onClick={(e)=>{this.handleAnswer(e, true)}} />
+          <input type="button" value="no" onClick={(e)=>{this.handleAnswer(e, false)}} />
+        </div>
+      </div>);
+  }
+
+}
+
+Question.propTypes = {
+  id: PropTypes.number.isRequired,
+  processAnswer: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+
+class OutOfQuestions extends React.Component {
+
+  render() {
+    return <p>I'm out of questions.</p>;
+  }
+
+}
+
+
 class App extends React.Component {
 
   render() {
-    return (<ul>{
-      this.props.questions.map((question) => {
-        return <li key={question.id}>{question.value}</li>;
-      })
-    }</ul>);
+    const current = this.props.current;
+    return (
+      <div>
+        <p id="prompt">Think of a number between 1 and 100. I'll try to guess it.</p>
+        <History entries={this.props.history} />
+        { current ?
+          <Question id={current.id}
+                    processAnswer={this.props.processAnswer}
+                    text={current.questionText} /> :
+          <OutOfQuestions /> }
+        <Upcoming entries={this.props.upcoming} />
+      </div>);
   }
 
 }
 
 App.propTypes = {
-  questions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  history: PropTypes.arrayOf(PropTypes.object).isRequired,
+  current: PropTypes.object,
+  processAnswer: PropTypes.func.isRequired,
+  upcoming: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 
@@ -28,25 +124,46 @@ class AppState extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      questions: [
-        { id: 1, value: 'What is 1 + 1?' },
-        { id: 2, value: 'What is 2 + 2?' },
+      history: [],
+      current: { id: 1, questionText: 'Is it even?', answerValue: true, score: 8 },
+      upcoming: [
+        { id: 2, questionText: 'Is it greater than 30?', answerValue: false, score: 6 },
+        { id: 3, questionText: 'Is it a prime?', score: 5 },
+        { id: 4, questionText: 'Is it 4?', score: 3 },
       ],
     };
   }
 
   componentDidMount() {
     this.subscriberId = events.subscribe(
-      'update-questions', (e) => this.handleQuestionUpdate(e));
+      'update-question-scores', (e) => this.handleStateUpdate(e));
   }
 
   componentWillUnmount() {
-    events.unsubscribe('update-questions', this.subscriberId);
+    events.unsubscribe('update-question-scores', this.subscriberId);
+  }
+
+  processAnswer(answerValue) {
+    const { history, current, upcoming } = this.state;
+    const newState = {
+      history: history.concat([{
+        id: current.id,
+        questionText: current.questionText,
+        score: current.score,
+        answerValue,
+      }]),
+      current: upcoming[0],
+      upcoming: upcoming.slice(1)
+    };
+    this.setState(newState);
   }
 
   render() {
     return (
-      <App questions={this.state.questions} />
+      <App current={this.state.current}
+           history={this.state.history}
+           processAnswer={this.processAnswer.bind(this)}
+           upcoming={this.state.upcoming} />
     );
   }
 }
